57c25a4c92569f6c800216c2bc44c2f4a2cfd149
kunal <kunalraut0009@gmail.com>
2026-07-22 23:47:26 +0530

    add microsoft clarity

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -73,6 +73,15 @@ export default function RootLayout({
       gtag('config', 'G-GN7L0T7GNZ');
     `}
   </Script>
+  <Script id="microsoft-clarity" strategy="afterInteractive">
+  {`
+    (function(c,l,a,r,i,t,y){
+      c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+      t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+      y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "xqjszdbsdn");
+  `}
+</Script>
 </body>
     </html>
   );
